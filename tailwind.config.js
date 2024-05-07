9d305fba281a59f7aadb558c81378e2f2348aabc
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-05-07 12:18:46 -0600

    sideBar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -37,5 +38,7 @@ module.exports = {
       neonBlue: 'rgb(77, 238, 234)',
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+],
 };
