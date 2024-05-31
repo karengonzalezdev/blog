01c381c1d1f0947ed7b4732358adbdf63093f41e
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-05-31 10:35:24 -0600

    Sping data added

--- src/app/spring/page.jsx
+++ src/app/spring/page.jsx
@@ -16,9 +16,9 @@ const SpringPage = () => {
         {/* TEXT */}
         <div className="text-white py-8 px-20 md:w-4/5 flex flex-col">
             <h1 className="text-3xl md:text-7xl text-center pb-5">Spring</h1>
-            <p className="text-justify text-xl md:text-2xl py-2"></p>
-            <p className="text-justify text-xl md:text-2xl py-2"></p>
-            <p className="text-justify text-xl md:text-2xl py-2"></p>
+            <p className="text-justify text-xl md:text-2xl py-2">Spring is a framework for the development of Java applications.</p>
+            <p className="text-justify text-xl md:text-2xl py-2">For the security, Spring needs to add both the standard spring-security-web and spring-security-config dependencies. And it also needs to add a class that creates a SecurityFilterChain bean, and make use of the annotation @EnableWebSecurity.</p>
+            <p className="text-justify text-xl md:text-2xl py-2">To add all the test libraries to our project, we need to add all of these as independent dependencies.</p>
         </div>
     
         </div>
--- src/app/springboot/page.jsx
+++ src/app/springboot/page.jsx
@@ -15,10 +15,10 @@ const SpringbootPage = () => {
     
         {/* TEXT */}
         <div className="text-white py-8 px-20 md:w-4/5 flex flex-col">
-            <h1 className="text-3xl md:text-7xl text-center bg-red-500 pb-5">SpringBoot</h1>
-            <p className="text-justify text-xl md:text-2xl py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium, distinctio iste, temporibus sit ipsa autem harum assumenda non quo eum tempora doloribus voluptas fugiat. Et corporis amet quidem nam.</p>
-            <p className="text-justify text-xl md:text-2xl py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est quia sit! Debitis nesciunt suscipit qui. Eaque omnis vitae neque magni est nostrum, optio itaque libero sint necessitatibus ab recusandae?</p>
-            <p className="text-justify text-xl md:text-2xl py-2"></p>
+            <h1 className="text-3xl md:text-7xl text-center pb-5">Spring Boot</h1>
+            <p className="text-justify text-xl md:text-2xl py-2">Spring Boot is a Java Framework, to run applications. It is an extension of Spring framework</p>
+            <p className="text-justify text-xl md:text-2xl py-2">For the security, we need to define the spring-boot-starter-security dependency, and this will add automatically all the relevant dependencies to the classpath. And it also needs to add a class that creates a SecurityFilterChain bean, and make use of the annotation @EnableWebSecurity.</p>
+            <p className="text-justify text-xl md:text-2xl py-2">To include testing libraries  within our project, we only need to add the starter dependency for testing, and this automatically includes in our project all the necessary libraries, such as Spring Test, JUnit, Hamcrest, and Mockito.</p>
         </div>
     
         </div>
